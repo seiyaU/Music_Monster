668c28e8a5733cef44ea40aae4a7f989d258e935
Ueda Seiya <seiya@UedanoMacBook-Pro.local>
2025-10-13 18:05:21 +0100

    work well

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def generate_image(user_id):
         "Content-Type": "application/json",
     }
 
-    MODEL_VERSION = "6a52feace43ce1f6bbc2cdabfc68423cb2319d7444a1a1dae529c5e88b976382"
+    MODEL_VERSION = "7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc"
 
     payload = {
         "version": MODEL_VERSION,
@@ -172,8 +172,7 @@ def generate_image(user_id):
             "image": image_data_uri,
             "strength": 0.6,
             "num_outputs": 1,
-            "width": 768,
-            "height": 1024
+            "aspect_ratio": "3:4"
         }
     }
 
