14233d2a91f84e5e62329a7c8420ac0bf4bc9d1a
Ueda Seiya <seiya@UedanoMacBook-Pro.local>
2025-10-13 18:24:36 +0100

    work well

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def generate_image(user_id):
         "Content-Type": "application/json",
     }
 
-    MODEL_VERSION = "7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc"
+    MODEL_VERSION = "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4"
 
     payload = {
         "version": MODEL_VERSION,
