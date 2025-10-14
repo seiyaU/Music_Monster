89964bf416fe40a660e697916b6f58730a179c0d
Ueda Seiya <seiya@UedanoMacBook-Pro.local>
2025-10-14 23:23:02 +0100

    add logic influenced by album image

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def generate_image(user_id):
         f"Legendary creature in {base_image_path} is a soldier with some weapons from a dark and mysterious world."
         f"It is influenced by the word of {influenced_word} "
         f"and designed like creepy monsters in SF or horror films but not cartoonish rather realistic. "
-        f"The background is made by things relevant to {album_image_url}."
+        f"influenced by {album_image_url}."
         ##f"Highly detailed, intricate, elegant, ornate, beautiful, epic, dramatic lighting, 8k, photorealistic"
     )
     print(prompt)
