afa90b506b3d2c2013e05b0d0cc78ebb8cbca875
Ueda Seiya <seiya@UedanoMacBook-Pro.local>
2025-10-15 15:24:11 +0100

    test

--- app.py
+++ app.py
@@ -169,13 +169,13 @@ def generate_image(user_id):
     print(character_animal)
     print(influenced_word)
     print(album_image_url)
+    #{base_image_path}
 
     prompt = (
-        f"Legendary creature in {base_image_path} is a soldier with some weapons from a dark and mysterious world."
+        f"Legendary creature in {album_image_url} is a soldier with some weapons from a dark and mysterious world."
         f"It is influenced by the word of {influenced_word} "
         f"and designed like creepy monsters in SF or horror films but not cartoonish rather realistic. "
-        f"influenced by {album_image_url}. "
-        f"Highly detailed, intricate, elegant, ornate, beautiful, epic, dramatic lighting, 8k, photorealistic"
+        f"influenced by {album_image_url}."
     )
     print(prompt)
 
